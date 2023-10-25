4aef20f410bae3727216fb531cb33be77ec45ae7
Thon <tonmarchetti.im@gmail.com>
2023-10-25 10:02:00 -0300

    criação

---
+++ conexao-html-js/script.js
@@ -0,0 +1,27 @@
+//console.log("Olá Mundo!")
+
+let logo = document.getElementById("logo");
+//console.log(logo)
+
+let postAutor = document.getElementsByClassName("post-autor");
+//console.log(postAutor)
+
+let texto02 = document.querySelector("#post02 .post-texto")
+//console.log(texto02)
+
+let redesFooter = document.querySelectorAll("footer .lista_redes li")
+
+
+
+
+
+
+//console.log(redesFooter)
+/*
+for(let i = 0; redesFooter.length; i++){
+    console.log("Lista elemento número: " + i)
+}
+
+for(let i = 0; postAutor.length; i++){
+    console.log(postAutor[i])
+}*/
