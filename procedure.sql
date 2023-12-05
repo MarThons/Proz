6ed84b1f657971b35b1852e2b1b86f962e9a07b5
Thon Marchetti <129338682+MarThons@users.noreply.github.com>
2023-12-05 18:17:53 -0300

    Update procedure.sql

--- procedure.sql
+++ procedure.sql
@@ -1,1 +1,11 @@
+CREATE OR REPLACE PROCEDURE comprasDia()
+AS
+$$
+BEGIN
+    SELECT produto, quantidade INTO produto_resultado, quantidade_resultado
+    FROM transacoes;
+END;
+$$ LANGUAGE plpgsql;
+
+
 call comprasDia();
