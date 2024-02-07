b40536deb2de782294dd593207c7b793d366bdde
Thon Marchetti <tonmarchetti.im@gmail.com>
2024-02-07 15:27:29 -0300

    REACT- DOM

--- JS-para-REACT/primeiro-projeto-react/src/App.jsx
+++ JS-para-REACT/primeiro-projeto-react/src/App.jsx
@@ -1,34 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="titulo">Arquivo.jsx</h1>
+      <p>Texto do arquivo.jsx</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, minima quae porro accusantium sapiente adipisci magni rerum minus vero quia.</p>
+      <label htmlFor="email">Seu E-mail</label>
+      <input type="email" id="email" />
+      <img src="" alt="" />
     </>
+
   )
 }
 
